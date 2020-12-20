b057b9c452fe9d56ee3812bcc3e25d4f6d949d98
PradeepPM007 <pm.pradeep.nh@gmail.com@gmail.com>
2020-12-20 23:55:35 +0530

    Not usable

--- Springboot-2-JPA/src/main/java/com/pm/demo/dao/AlienRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pm.demo.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.pm.demo.Alien;
-
-public interface AlienRepo extends CrudRepository<Alien, Long>{
-
-}
